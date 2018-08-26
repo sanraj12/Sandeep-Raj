705407050f7467f6c4ab86884e06128daeccaedc
sanraj12 <sandeep.raj0304@gmail.com>
2018-08-26 13:06:23 +0530

    Practice

---
+++ Practice/src/PassByValue.java
@@ -0,0 +1,47 @@
+
+public class PassByValue {
+
+	public static void tricky(Point arg1, Point arg2)
+	{
+	  arg1.x = 100;
+	  arg1.y = 100;
+	  Point temp = arg1;
+	  arg1 = arg2;
+	  arg2 = temp;
+	}
+	
+	static void chcek(int d)
+	{
+		d=8;
+	}
+	public static void main(String [] args)
+	{
+		int a = 7;
+		
+		chcek(a);
+		System.out.println(a);
+		
+		
+		
+	  Point pnt1 = new Point(0,0);
+	  Point pnt2 = new Point(0,0);
+	  System.out.println("X: " + pnt1.x + " Y: " +pnt1.y); 
+	  System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);
+	  System.out.println(" ");
+	  tricky(pnt1,pnt2);
+	  System.out.println("X: " + pnt1.x + " Y:" + pnt1.y); 
+	  System.out.println("X: " + pnt2.x + " Y: " +pnt2.y);  
+	}
+
+}
+
+class Point{
+	int x= 0;
+	int y = 0;
+	Point(int x,int y)
+	{
+		x=x;
+		y=y;
+		
+	}
+}
